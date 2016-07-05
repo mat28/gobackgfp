6ccab0e5e7ba420a8eafbcc6b6056d0a7d584896
rxmat <mathieu.roux21@gmail.com>
2016-07-05 11:51:00 +0200

    ! helloController response write in json

--- controllers/helloController.go
+++ controllers/helloController.go
@@ -3,8 +3,20 @@ package controllers
 
 import (
 	"net/http"
+	"encoding/json"
+	"log"
 )
 
-func HelloController(w http.ResponseWriter, r *http.Request, next http.HandlerFunc){
 
+func HelloController(w http.ResponseWriter, r *http.Request, next http.HandlerFunc){
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(200)
+	data := struct {
+		Message string
+	}{
+		"Hello World",
+	}
+	if err := json.NewEncoder(w).Encode(data);err != nill {
+		log.Println(err)
+	}
 }
